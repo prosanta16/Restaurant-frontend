5e3b74f40c0be1d3a3f5cb32ff07c68b961a9a36
Prosanta Paul <prosantapaulbsmrstu@gmail.com>
2021-01-04 01:10:40 +0600

    css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './plugins/bootstrap-vue.js'
+
+// import './assets/css/icofont.min.css'
+// import './assets/css/boxicons.min.css'
+// import './assets/css/animate.min.css'
+// import './assets/css/owl.carousel.min.css'
+// import './assets/css/venobox.css'
+// import './assets/css/aos.css'
+
+
 Vue.config.productionTip = false
 
 new Vue({
